c70e9b639fdc83232a43e27cafb4461e9bee82dd
Luca Milanesio <luca.milanesio@gmail.com>
2023-09-23 13:16:47 +0100

    Introduce defaultImage when the account does not have emails
    When the Gerrit account did not have any emails, the avatar
    was null which was creating strange situations where the icon
    was taken from other accounts on the GUI.
    
    Return always the Gravatar default image, so that it would be
    clear that the user does not have a visual identity and would
    not be assigned to other people's faces.
    
    Change-Id: Ia5bde853ebbb4a48bc30db30a6f9f47dfe68d935

---
+++ src/test/java/com/googlesource/gerrit/plugins/avatars/gravatar/GravatarAvatarProviderIT.java
@@ -0,0 +1,60 @@
+// Copyright (C) 2023 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.avatars.gravatar;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.gerrit.acceptance.LightweightPluginDaemonTest;
+import com.google.gerrit.acceptance.TestPlugin;
+import com.google.gerrit.acceptance.UseLocalDisk;
+import com.google.gerrit.acceptance.config.GerritConfig;
+import com.google.gerrit.entities.Account;
+import com.google.gerrit.server.IdentifiedUser;
+import com.google.gerrit.server.PropertyMap;
+import com.google.inject.AbstractModule;
+import com.google.inject.Inject;
+import org.junit.Test;
+
+@UseLocalDisk
+@TestPlugin(
+    name = "avatars-gravatar",
+    sysModule =
+        "com.googlesource.gerrit.plugins.avatars.gravatar.GravatarAvatarProviderIT$TestModule")
+public class GravatarAvatarProviderIT extends LightweightPluginDaemonTest {
+  private static final int IMAGE_SIZE = 100;
+
+  @Inject IdentifiedUser.GenericFactory userFactory;
+
+  public static class TestModule extends AbstractModule {}
+
+  @Test
+  public void accountWithoutEmailShouldHaveImageByDefault() {
+    assertThat(gravatarAvatar().getUrl(testUserWithoutEmail(), IMAGE_SIZE)).isNotNull();
+  }
+
+  @Test
+  @GerritConfig(name = "plugin.avatars-gravatar.defaultImage", value = "false")
+  public void accountWithoutEmailShouldDisableDefaultImage() {
+    assertThat(gravatarAvatar().getUrl(testUserWithoutEmail(), IMAGE_SIZE)).isNull();
+  }
+
+  private GravatarAvatarProvider gravatarAvatar() {
+    return plugin.getSysInjector().getInstance(GravatarAvatarProvider.class);
+  }
+
+  private IdentifiedUser testUserWithoutEmail() {
+    return userFactory.forTest(Account.id(1), PropertyMap.EMPTY);
+  }
+}
